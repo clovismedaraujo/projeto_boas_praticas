af21c19d5fcc04eac9a37fb0148df6d846b2233c
Clóvis Luan <luanmdearaujo@gmail.com>
2025-11-09 20:53:49 -0300

    Adicionando a classe Conta
    Introduz a classe abstrata Conta, com as operações de deposito, transferencia e saque.

---
+++ Conta.java
@@ -0,0 +1,62 @@
+package projeto_boas_praticas;
+
+
+public abstract class Conta {
+    private String numeroConta; 
+    private Cliente cliente;
+    protected double saldoConta; 
+    private static final double SALDO_INICIAL_PADRAO = 0.0;
+
+    public Conta(String numero, Cliente cliente, double saldoInicial) {
+        this.numeroConta = numero;
+        this.cliente = cliente;
+        this.saldoConta = saldoInicial;
+    }
+
+    public Conta(String numero, Cliente cliente) {
+        this.numeroConta = numero;
+        this.cliente = cliente;
+        this.saldoConta = SALDO_INICIAL_PADRAO;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.saldoConta += valor;
+        }
+    }
+
+
+    public boolean sacar(double valor) {
+        if (valor > 0 && this.saldoConta >= valor) {
+            this.saldoConta -= valor;
+            return true;
+        }
+  
+        return false;
+    }
+
+ 
+
+
+    public double getSaldoConta() {
+        return saldoConta;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+
+    public abstract String getTipoConta();
+
+   
+    @Override
+    public String toString() {
+        return String.format("Conta %s %s | Cliente: %s | Saldo: R$ %.2f",
+                             getTipoConta(), numeroConta, cliente.getNomeCliente(), saldoConta);
+    }
+}
